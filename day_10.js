a63d0db26504eb6c404e8896aca68738aaa9432b
daffaalex22 <daffaalex22@gmail.com>
2021-11-01 11:44:00 +0700

    delete line break under thanks

--- day_10.js
+++
@@ -1,8 +0,0 @@
-// function submitForm() {
-//     oForm = document.forms[0];
-//     fullName = oForm.elements["fullname"].value;
-//     emailAddress = oForm.elements["email"].value;
-//     phoneNumber = oForm.elements["phone"].value;
-//     nationality = oForm.elements["nationality"].value;
-//     message = oForm.elements["message"].value;
-// }
