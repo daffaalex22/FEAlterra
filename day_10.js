c1633d1b56cd48de2fc8b607639081f89f1347d9
daffaalex22 <daffaalex22@gmail.com>
2021-11-01 11:34:48 +0700

    add task day 10

---
+++ day_10.js
@@ -0,0 +1,8 @@
+// function submitForm() {
+//     oForm = document.forms[0];
+//     fullName = oForm.elements["fullname"].value;
+//     emailAddress = oForm.elements["email"].value;
+//     phoneNumber = oForm.elements["phone"].value;
+//     nationality = oForm.elements["nationality"].value;
+//     message = oForm.elements["message"].value;
+// }
