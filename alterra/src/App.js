db4ecbfcd77c0d9cd17fcf7dbec34da7b30677dd
daffaalex22 <daffaalex22@gmail.com>
2021-11-06 04:00:57 +0700

    add close navbar

--- alterra/src/App.js
+++ alterra/src/App.js
@@ -11,16 +11,26 @@ import Navbar from './components/Navbar/Navbar';
 
 //STUFF FROM REACT
 import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
+  const [navIsOpen, setNavIsOpen] = useState(true)
+
+  const handleCloseNav = () => {
+    setNavIsOpen(false)
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path="/"><Navbar component={<Todo />} tab='home' /></Route>
-          <Route path="/about-author"><Navbar component={<AboutAuthor />} tab='about-author' /></Route>
-          <Route path="/about-app"><Navbar component={<AboutApp />} tab='about-app' /></Route>
+          <Route exact path="/"><Navbar component={<Todo />}
+            tab='home' closeNav={handleCloseNav} navIsOpen={navIsOpen} /></Route>
+          <Route path="/about-author"><Navbar component={<AboutAuthor />}
+            tab='about-author' closeNav={handleCloseNav} navIsOpen={navIsOpen} /></Route>
+          <Route path="/about-app"><Navbar component={<AboutApp />}
+            tab='about-app' closeNav={handleCloseNav} navIsOpen={navIsOpen} /></Route>
           <Route path="*"><NotFound /></Route>
         </Switch>
       </div >
--- alterra/src/components/Navbar/Navbar.js
+++ alterra/src/components/Navbar/Navbar.js
@@ -1,22 +1,39 @@
-import './Navbar.css'
+import './Navbar.css';
 import { Link } from 'react-router-dom';
-import { FaMinus } from 'react-icons/fa'
+import { useState } from 'react';
+import { FaMinus } from 'react-icons/fa';
 
-const Navbar = ({ component }) => {
+const Navbar = ({ component, tab, closeNav, navIsOpen }) => {
+
     return (
         <div className="navbar align-items-start position-fixed">
-
             <div className="d-flex align-items-start ">
-                <button className="btn close-btn position-fixed"><FaMinus className="close-icon position-fixed justify-content-end" /></button>
-                <div className="nav flex-column position-sticky nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <Link className="btn btn-outline-primary nav-link" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" to="/">Home</Link>
-                    <Link className="btn btn-outline-primary nav-link" id="v-pills-about-app-tab" data-bs-toggle="pill" data-bs-target="#v-pills-about-app" type="button" role="tab" aria-controls="v-pills-about-app" aria-selected="true" to="/about-app">About App</Link>
-                    <Link className="btn btn-outline-primary nav-link active" id="v-pills-about-author-tab" data-bs-toggle="pill" data-bs-target="#v-pills-about-author" type="button" role="tab" aria-controls="v-pills-about-author" aria-selected="false" to="/about-author">About Author</Link>
-                </div>
+                {navIsOpen &&
+                    <div className="sidebar">
+                        <button className="btn close-btn position-fixed" onClick={closeNav}>
+                            <FaMinus className="close-icon position-fixed justify-content-end" />
+                        </button>
+                        <div className="nav flex-column position-sticky nav-pills me-3" id="v-pills-tab" role="tablist"
+                            aria-orientation="vertical">
+                            <Link className={`btn btn-outline-primary nav-link ${tab == 'home' ? 'active' : ''}`}
+                                id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button"
+                                role="tab" aria-controls="v-pills-home" to="/">Home</Link>
+                            <Link className={`btn btn-outline-primary nav-link ${tab == 'about-app' ? 'active' : ''}`}
+                                id="v-pills-about-app-tab" data-bs-toggle="pill" data-bs-target="#v-pills-about-app" type="button"
+                                role="tab" aria-controls="v-pills-about-app" to="/about-app">About App</Link>
+                            <Link className={`btn btn-outline-primary nav-link ${tab == 'about-author' ? 'active' : ''}`}
+                                id="v-pills-about-author-tab" data-bs-toggle="pill" data-bs-target="#v-pills-about-author" type="button"
+                                role="tab" aria-controls="v-pills-about-author" to="/about-author">About Author</Link>
+                        </div>
+                    </div>
+                }
                 <div className="tab-content" id="v-pills-tabContent">
-                    <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">{component}</div>
-                    <div className="tab-pane fade" id="v-pills-about-app" role="tabpanel" aria-labelledby="v-pills-about-app-tab">{component}</div>
-                    <div className="tab-pane fade" id="v-pills-about-author" role="tabpanel" aria-labelledby="v-pills-about-author-tab">{component}</div>
+                    <div className="tab-pane fade show active" id="v-pills-home"
+                        role="tabpanel" aria-labelledby="v-pills-home-tab">{component}</div>
+                    <div className="tab-pane fade" id="v-pills-about-app"
+                        role="tabpanel" aria-labelledby="v-pills-about-app-tab">{component}</div>
+                    <div className="tab-pane fade" id="v-pills-about-author"
+                        role="tabpanel" aria-labelledby="v-pills-about-author-tab">{component}</div>
                 </div>
             </div>
         </div >
