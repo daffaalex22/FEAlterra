5fc232469d861272c6d340bbb74a49e83724316e
daffaalex22 <daffaalex22@gmail.com>
2021-11-16 14:31:57 +0700

    selesai Day_21

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,82 +1,195 @@
-import { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
 import PassengerInput from './PassengerInput';
 import ListPassenger from './ListPassenger';
 import Header from './Header';
+import { useState, useEffect } from "react";
+import { gql, useLazyQuery, useQuery } from '@apollo/client'
 
-class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            data : [
-                {
-                    id: uuidv4(),
-                    nama: 'Yoga',
-                    umur: 22,
-                    jenisKelamin: 'Pria'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Ria',
-                    umur: 19,
-                    jenisKelamin: 'Wanita'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Fahmi',
-                    umur: 25,
-                    jenisKelamin: 'Pria'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Lala',
-                    umur: 21,
-                    jenisKelamin: 'Wanita'
-                },
-                {
-                    id: uuidv4(),
-                    nama: 'Ivan',
-                    umur: 25,
-                    jenisKelamin: 'Pria'
-                }
-            ]
+
+const Home = () => {
+
+    const [pengunjung, setPengunjung] = useState([]);
+
+
+    const GET_DATA = gql`
+    query MyQuery {
+        anggota {
+          id
+          nama
+          umur
+          jenis_kelamin
         }
+      }`
+
+    const GET_DATA_BYID = gql`
+    query MyQuery($_id: Int!) {
+        anggota(where: {id: {_eq: $_id}}) {
+          nama
+          umur
+          jenis_kelamin
+        }
+      }`
+
+    const [variables, setVariables] = useState({
+        variables: {
+            "_id": 1
+        }
+    })
+
+    // var allData = null
+
+    const { loading: allLoading, error: allError, data: allData } = useQuery(GET_DATA);
+    const [getById, { loading: singleLoading, error: singleError, data: singleData }] = useLazyQuery(GET_DATA_BYID, variables);
+
+    useEffect(() => {
+        if (allData) {
+            setPengunjung(allData.anggota)
+        }
+    }, [allData])
+
+    useEffect(() => {
+        if (singleData) {
+            setPengunjung(singleData.anggota)
+        }
+    }, [singleData])
+
+
+    if (allLoading) {
+        return 'Loading...';
     }
 
-    hapusPengunjung = id => {
-        this.setState({    
-            data: [      
-                ...this.state.data.filter(item => {        
-                    return item.id !== id;      
-                })    
-            ]  
-        });
+    if (allError) {
+        return `Error! ${allError.message}`;
+    }
+
+    const hapusPengunjung = id => {
+        setPengunjung((prevState) => (
+            [...prevState.filter(item => {
+                return item.id !== id;
+            })]
+        ))
     };
-    
-    tambahPengunjung = newUser => {
+
+    const tambahPengunjung = newUser => {
         const newData = {
             id: uuidv4(),
             ...newUser
-        }; 
-        this.setState({    
-            data: [...this.state.data, newData]  
-        });
+        };
+        setPengunjung((prevState) => (
+            [...prevState, newData]
+        ))
     };
-    
-    render() {
-        return (
-            <div>
-                <Header/>
-                <ListPassenger 
-                    data={this.state.data}
-                    hapusPengunjung={this.hapusPengunjung}
-                />
-                <PassengerInput
-                    tambahPengunjung={this.tambahPengunjung}
-                />
-            </div>
-        )
+
+    const handleChange = (e) => {
+        setVariables((prevState) => ({
+            ...prevState,
+            variables: {
+                "_id": e.target.value
+            }
+        }))
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        getById(variables)
+    }
+
+    return (
+        <div>
+            <Header />
+            <form>
+                <input type="number" name="_id" onChange={handleChange} />
+                <button type="submit" onClick={handleSubmit}>Search by Id</button>
+            </form>
+            <br />
+            <ListPassenger
+                data={pengunjung}
+                hapusPengunjung={hapusPengunjung}
+            />
+            <PassengerInput
+                tambahPengunjung={tambahPengunjung}
+            />
+        </div>
+    );
 }
 
 export default Home;
+
+
+// class Home extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             data : [
+//                 {
+//                     id: uuidv4(),
+//                     nama: 'Yoga',
+//                     umur: 22,
+//                     jenisKelamin: 'Pria'
+//                 },
+//                 {
+//                     id: uuidv4(),
+//                     nama: 'Ria',
+//                     umur: 19,
+//                     jenisKelamin: 'Wanita'
+//                 },
+//                 {
+//                     id: uuidv4(),
+//                     nama: 'Fahmi',
+//                     umur: 25,
+//                     jenisKelamin: 'Pria'
+//                 },
+//                 {
+//                     id: uuidv4(),
+//                     nama: 'Lala',
+//                     umur: 21,
+//                     jenisKelamin: 'Wanita'
+//                 },
+//                 {
+//                     id: uuidv4(),
+//                     nama: 'Ivan',
+//                     umur: 25,
+//                     jenisKelamin: 'Pria'
+//                 }
+//             ]
+
+//         }
+//     }
+
+//     hapusPengunjung = id => {
+//         this.setState({    
+//             data: [      
+//                 ...this.state.data.filter(item => {        
+//                     return item.id !== id;      
+//                 })    
+//             ]  
+//         });
+//     };
+
+//     tambahPengunjung = newUser => {
+//         const newData = {
+//             id: uuidv4(),
+//             ...newUser
+//         }; 
+//         this.setState({    
+//             data: [...this.state.data, newData]  
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <Header/>
+//                 <ListPassenger 
+//                     data={this.state.data}
+//                     hapusPengunjung={this.hapusPengunjung}
+//                 />
+//                 <PassengerInput
+//                     tambahPengunjung={this.tambahPengunjung}
+//                 />
+//             </div>
+//         )
+//     }
+// }
+
+// export default Home;
--- src/component/ListPassenger.js
+++ src/component/ListPassenger.js
@@ -1,16 +1,21 @@
 import ListItem from './ListItem';
+import useEffect from 'react'
+
 const ListPassenger = props => {
-    
+
+    console.log(props.data)
+
     return (
         <div>
-            <table cellPadding="5px" cellSpacing="0" style={{margin: "auto"}}>
+            <table cellPadding="5px" cellSpacing="0" style={{ margin: "auto" }}>
                 <thead bgcolor="red">
                     <td>Nama</td>
                     <td>Umur</td>
                     <td>Jenis Kelamin</td>
                     <td bgcolor="white" className="removeBorder"></td>
                 </thead>
-                {props.data.map(item => (
+
+                {props.data && props.data.map(item => (
                     <ListItem
                         key={item.id}
                         data={item}
@@ -20,6 +25,6 @@ const ListPassenger = props => {
             </table>
         </div>
     )
-  }
+}
 
 export default ListPassenger;
